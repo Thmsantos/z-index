962f1ffeda86b360c3179fa4fda285772383b12b
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2022-10-26 23:35:02 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div id='bloco1'>
+        <p>Bloco 01</p>
+     </div>
+     <div id='bloco2'>
+        <p>Bloco 02</p>
+     </div>
+     <div id='bloco3'>
+        <p>Bloco 03</p>
+     </div>
+     <div id='bloco4'>
+        <p>Bloco 04</p>
+     </div>
+     <div id='bloco5'>
+        <p>Bloco 05</p>
+     </div>
+     <div id='bloco6'>
+        <p>Bloco 06</p>
+     </div>
+     <div id='bloco7'>
+        <p>Bloco 07</p>
+     </div>
+     <div id='bloco8'>
+        <p>Bloco 08</p>
+     </div>
+     <div id='bloco9'>
+        <p>Bloco 09</p>
+     </div>
+     <div id='bloco10'>
+        <p>Bloco 10</p>
+     </div>
+     <div id='bloco11'>
+        <p>Bloco 11</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
